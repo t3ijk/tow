34de84c223917f6b25a5f3ff1b00cc456872b4bb
wins <wins@pc>
2023-12-22 11:18:33 +0800

    update

--- app.py
+++ app.py
@@ -17,6 +17,9 @@ def translate(model_path, inputs):
     model = model.eval()
     tokenizer = Tokenizer_byt5()  
     ids = tokenizer(inputs, max_length=1024)
+    len_pad = 1024 - len(ids)
+    if len_pad > 0:
+        ids = ids + [0 for x in range(len_pad)]
     print(ids)  
     inputs = torch.tensor([ids]).to(torch.device('cpu'))
     outputs = model.generate(inputs, max_length=1024)
